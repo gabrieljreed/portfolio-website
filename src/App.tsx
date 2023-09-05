5df4b2d7fae55848cfe1894b502781162a56851c
Gabriel Reed <gabrieljreed@gmail.com>
2023-09-04 22:04:53 -0600

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { Route, Routes } from "react-router-dom";
+import './App.css'
 
 import Home from "./pages/HomePage";
 import About from "./pages/AboutPage";
 import Contact from "./pages/ContactPage";
 import Projects from "./pages/ProjectsPage";
-import './App.css'
+import Navbar from "./pages/Navbar";
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <Routes>
 
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
 
       </Routes>
     </>

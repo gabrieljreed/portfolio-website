86cbe810714c8396dffc51f8654d7881f74b2001
Gabriel Reed <gabrieljreed@gmail.com>
2023-09-10 11:51:11 -0600

    update navbar, homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,25 @@ function Home() {
   return (
     <>
       <h1>Home</h1>
-      <p>This is the home page</p>
+
+      <div className="flex flex-col items-center justify-center text-left">
+        <p className="">
+          I am a Pipeline Technical Director and Software Developer dedicated to elevating the creative process within animation studios or any creative team. With a passion for technology and a deep understanding of the animation pipeline, I specialize in designing and implementing efficent solutions that streamline production workflows.
+        </p>
+
+        <p className="">
+          My journey in the world of animation technology has led me to craft innovative pipelines, develop robust asset management systems, and create tools that empower artists. Through my work, I strive to bridge the gap between artistry and technology, ensuring that studios can bring their imaginative visions to life efficiently and seamlessly.
+        </p>
+
+        <p className="">
+          Thank you for visiting, and I look forward to connecting with you to discuss how I can contribute to your next creative endeavor.
+        </p>
+
+        {/* <p className=""></p>
+          Explore my portfolio to discover a showcase of projects and collaborations that reflect my commitment to excellence in animation technology. From custom tools to intuitive interfaces, I am here to transform challenges into opportunities and contribute to the vibrant world of animation.
+        </p> */}
+
+      </div>
     </>
   );
 }
